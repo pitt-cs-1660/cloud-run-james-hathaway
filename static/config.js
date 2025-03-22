ed40ac8d0834c8de27951d5b563eaf82d1dbfb8b
JRH230 <jrh230@pitt.edu>
2025-03-22 14:05:55 -0400

    added firebase stuff probably wrong

--- static/config.js
+++ static/config.js
@@ -13,9 +13,17 @@
  * Firebase configuration
  * @type {{apiKey: string, authDomain: string}}
  */
+
+import { initializeApp } from "firebase/app";
 const config = {
-  apiKey: "AIzaSyByTo0_tg-ZdGIZFRIFQIsH9ZR917iDdCU",
+  apiKey: "AIzaSyD9LaQkHUBpyxYjRxF1fE_8soGVsNeuJh8",
   authDomain: "cloud-run-project-454317.firebaseapp.com",
+  projectId: "cloud-run-project-454317",
+  storageBucket: "cloud-run-project-454317.firebasestorage.app",
+  messagingSenderId: "164008081777",
+  appId: "1:164008081777:web:3a19cb0486b17d71179ed0"
 };
+
+const app = initializeApp(config);
 
 export default config;
